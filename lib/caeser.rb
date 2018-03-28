8fb126d749d02896b66ee52a24d804c484d98ca5
Samuel Masters <redeyetuning@gmail.com>
2018-03-28 19:56:06 +0400

    Updater caeser.rb to have include a Caeser_cipher Class with .encode method

--- lib/caeser.rb
+++ lib/caeser.rb
@@ -5,41 +5,43 @@
 #of positions down the alphabet. For example, with a left shift of 3, D would be 
 #replaced by A, E would become B, and so on. The method is named after Julius 
 #Caesar, who used it in his private correspondence.
+class Caeser_cipher
+	def self.encode string, shift
+		output = []
 
-def caeser_cipher string, shift
-	output = []
+		string.split("").each do |char|
+			i = char.ord
+			if i >= 65 && i <= 90
+				if i + shift > 90
+					output << (char.ord + shift - 26).chr
+				elsif i + shift < 65
+					output << (char.ord + shift + 26).chr
+				else
+					output << (char.ord + shift).chr
+				end
 
-	string.split("").each do |char|
-		i = char.ord
-		if i >= 65 && i <= 90
-			if i + shift > 90
-				output << (char.ord + shift - 26).chr
-			elsif i + shift < 65
-				output << (char.ord + shift + 26).chr
+			elsif i >= 97 && i <= 122
+				if i + shift > 122
+					output << (char.ord + shift - 26).chr
+				elsif i + shift < 97
+					output << (char.ord + shift + 26).chr
+				else
+					output << (char.ord + shift).chr
+				end
 			else
-				output << (char.ord + shift).chr
-			end
+				output << char
 
-		elsif i >= 97 && i <= 122
-			if i + shift > 122
-				output << (char.ord + shift - 26).chr
-			elsif i + shift < 97
-				output << (char.ord + shift + 26).chr
-			else
-				output << (char.ord + shift).chr
 			end
-		else
-			output << char
-
 		end
+
+		puts output.join
 	end
 
-	puts output.join
+	puts "Enter string to encrypt:"
+	phrase = gets.chomp
+	puts "Enter numerical shift factor:"
+	shift = gets.chomp.to_i
+
+	self.encode(phrase, shift)
+
 end
-
-puts "Enter string to encrypt:"
-phrase = gets.chomp
-puts "Enter numerical shift factor:"
-shift = gets.chomp.to_i
-
-caeser_cipher(phrase, shift)
